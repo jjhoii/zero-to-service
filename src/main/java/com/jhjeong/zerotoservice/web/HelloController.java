47a2742682095d7cad27475fb801629b80fd6ddc
jhjeong <kkiu1756@gmail.com>
2021-07-15 19:37:08 +0900

    간단한 컨트롤러, 테스트 코드 작성

---
+++ src/main/java/com/jhjeong/zerotoservice/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.jhjeong.zerotoservice.web;
+
+import com.jhjeong.zerotoservice.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}

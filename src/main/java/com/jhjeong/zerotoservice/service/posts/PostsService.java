bb495b84d4d337d39e1bc055ab95ed5703c57842
jhjeong <kkiu1756@gmail.com>
2021-07-18 19:34:52 +0900

    게시판 기본 기능 구현

--- src/main/java/com/jhjeong/zerotoservice/service/posts/PostsService.java
+++ src/main/java/com/jhjeong/zerotoservice/service/posts/PostsService.java
@@ -2,9 +2,12 @@ package com.jhjeong.zerotoservice.service.posts;
 
 import com.jhjeong.zerotoservice.domain.posts.Posts;
 import com.jhjeong.zerotoservice.domain.posts.PostsRepository;
+import com.jhjeong.zerotoservice.web.dto.PostsListResponseDto;
 import com.jhjeong.zerotoservice.web.dto.PostsResponseDto;
 import com.jhjeong.zerotoservice.web.dto.PostsSaveRequestDto;
 import com.jhjeong.zerotoservice.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,10 +33,25 @@ public class PostsService {
     return id;
   }
 
+  @Transactional(readOnly = true)
   public PostsResponseDto findById(Long id) {
     Posts entity = postsRepository.findById(id).orElseThrow(() ->
         new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
     return new PostsResponseDto(entity);
+  }
+
+  @Transactional(readOnly = true)
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+        .collect(Collectors.toList());
+  }
+
+  @Transactional
+  public void delete(Long id) {
+    Posts posts = postsRepository.findById(id).orElseThrow(() ->
+        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+    postsRepository.delete(posts);
   }
 }

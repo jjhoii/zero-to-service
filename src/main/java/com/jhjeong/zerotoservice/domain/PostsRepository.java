a9b6a9686507a1ee15551e3ca12fad83c5a3c30d
jhjeong <kkiu1756@gmail.com>
2021-07-18 12:42:06 +0900

    Posts, repository, test 작성

---
+++ src/main/java/com/jhjeong/zerotoservice/domain/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.jhjeong.zerotoservice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
---
+++ src/test/java/com/jhjeong/zerotoservice/domain/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.jhjeong.zerotoservice.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PostsRepositoryTest {
+
+  @Autowired
+  PostsRepository postsRepository;
+
+  @AfterEach
+  public void cleanup() {
+    postsRepository.deleteAll();
+  }
+
+  @Test
+  public void 게시글저장_불러오기() {
+    // given
+    String title = "테스트 게시글";
+    String content = "테스트 본문";
+
+    postsRepository.save(Posts.builder()
+        .title(title)
+        .content(content)
+        .author("kkiu1756@gmail.com")
+        .build());
+
+    // when
+    List<Posts> postsList = postsRepository.findAll();
+
+    // then
+    Posts posts = postsList.get(0);
+    assertThat(posts.getTitle()).isEqualTo(title);
+    assertThat(posts.getContent()).isEqualTo(content);
+  }
+}

79c2b343fdeb77bf3660d9e5dda730280325e110
jhjeong <kkiu1756@gmail.com>
2021-07-18 14:22:37 +0900

    Post API 수정, 조회 기능 구현

--- src/main/java/com/jhjeong/zerotoservice/web/PostsApiController.java
+++ src/main/java/com/jhjeong/zerotoservice/web/PostsApiController.java
@@ -1,9 +1,14 @@
 package com.jhjeong.zerotoservice.web;
 
 import com.jhjeong.zerotoservice.service.posts.PostsService;
+import com.jhjeong.zerotoservice.web.dto.PostsResponseDto;
 import com.jhjeong.zerotoservice.web.dto.PostsSaveRequestDto;
+import com.jhjeong.zerotoservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +21,15 @@ public class PostsApiController {
   @PostMapping("/api/v1/posts")
   public Long save(@RequestBody PostsSaveRequestDto requestDto) {
     return postsService.save(requestDto);
+  }
+
+  @PutMapping("/api/v1/posts/{id}")
+  public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+    return postsService.update(id, requestDto);
+  }
+
+  @GetMapping("/api/v1/posts/{id}")
+  public PostsResponseDto findById(@PathVariable Long id) {
+    return postsService.findById(id);
   }
 }

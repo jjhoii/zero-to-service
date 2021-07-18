8c6736ae6e5076eef42aa683fff934fcb7bfe72b
jhjeong <kkiu1756@gmail.com>
2021-07-18 13:43:50 +0900

    Post API 등록 기능 구현

---
+++ src/main/java/com/jhjeong/zerotoservice/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jhjeong.zerotoservice.service.posts;
+
+import com.jhjeong.zerotoservice.domain.PostsRepository;
+import com.jhjeong.zerotoservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+  private final PostsRepository postsRepository;
+
+  @Transactional
+  public Long save(PostsSaveRequestDto requestDto) {
+    return postsRepository.save(requestDto.toEntity()).getId();
+  }
+}
